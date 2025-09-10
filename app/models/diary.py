804fbfaa2e05fd4aa203a51519c4c68a77f0f530
UNGBI78 <mybluewings78@gmail.com>
2025-09-10 17:21:46 +0900

    [✨feat] diary - 다이어리 기능 추가

---
+++ app/models/diary.py
@@ -0,0 +1,33 @@
+import enum
+from tortoise import fields
+from tortoise.models import Model
+
+
+class MoodEnum(str, enum.Enum):
+    HAPPY = "기쁨"
+    SAD = "슬픔"
+    CALM = "평온"
+    ANGRY = "분노"
+
+
+class WeatherEnum(str, enum.Enum):
+    SUNNY = "맑음"
+    CLOUDY = "흐림"
+    RAINY = "비옴"
+    SNOWY = "눈옴"
+
+
+class Diary(Model):
+    diary_key = fields.IntField(pk=True)
+    user = fields.ForeignKeyField("models.User", related_name="diaries")
+
+    title = fields.CharField(max_length=100)
+    content = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+    update_at = fields.DatetimeField(auto_now=True)
+
+    mood = fields.CharEnumField(MoodEnum, null=True)
+    weather = fields.CharEnumField(WeatherEnum, null=True)
+
+    class Meta:
+        table = "diary"
